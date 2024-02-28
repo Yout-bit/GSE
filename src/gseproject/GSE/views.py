5102e1def2fa4dc3422aade39d1efd483c314ee1
ncheng <150050119+nchengg@users.noreply.github.com>
2024-02-28 16:59:33 +0000

    Delete src/gseproject/GSE directory

--- src/gseproject/GSE/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import UserProfile
-
-def game_view(request):
-    user_profile = UserProfile.objects.get(user=request.user)
-    context = {'coins': user_profile.coins}
-    return render(request, 'your_template.html', context)
