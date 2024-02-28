27f904cd7b9334b34aff9ac82afc865cb454da10
Andrewexeter <129608903+Andrewexeter@users.noreply.github.com>
2024-02-28 17:42:04 +0000

    Update urls.py

--- GSE/EXEplorer/urls.py
+++ GSE/EXEplorer/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.home_view, name="home"),
     path("signup/", views.signup_view, name="signup"),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout")
