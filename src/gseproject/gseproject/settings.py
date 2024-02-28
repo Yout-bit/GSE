50c14fa96a17d7daede5242f0f714fe22b410a03
Yout-bit <stormcreeper18@gmail.com>
2024-02-28 19:48:20 +0000

    Add STATIC_ROOT

--- src/gseproject/gseproject/settings.py
+++ src/gseproject/gseproject/settings.py
@@ -120,6 +120,8 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [ BASE_DIR / 'static' ]
 
+STATIC_ROOT = BASE_DIR / 'res' 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
