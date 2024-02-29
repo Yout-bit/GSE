13290da90b113098f3a4fe82ed1d1141c3c24f02
Yout-bit <stormcreeper18@gmail.com>
2024-02-29 02:59:01 +0000

    Re-organise templates

--- src/gseproject/EXEplorer/urls.py
+++ src/gseproject/EXEplorer/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path("", views.home_view, name="home"),
     path('game', views.game_view, name='game'),
-    path("scanner", views.scanner_view, name="scanner")
 ]
--- src/gseproject/EXEplorer/views.py
+++ src/gseproject/EXEplorer/views.py
@@ -5,10 +5,6 @@ from django.http import HttpResponse
 
 def home_view(request):
     return render(request, 'home.html')
-    
-
-def scanner_view(request):
-    return render(request, 'game/qr_scanner.html')
 
 def game_view(request):
     try:
@@ -18,4 +14,4 @@ def game_view(request):
         # If the UserProfile does not exist, create a new one
         user_profile = UserProfile.objects.create(user=request.user)
     context = {'coins': user_profile.coins}
-    return render(request, 'temp scan and main.html', context)
+    return render(request, 'game/temp scan and main.html', context)
