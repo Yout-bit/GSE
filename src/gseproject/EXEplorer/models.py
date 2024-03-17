d9d75ea651efdd972ed7afb40aec01ea86d941f4
Isaacso <wss207@exeter.ac.uk>
2024-03-17 20:22:54 +0000

    The register page is working right now
    The register page is working right now

--- src/gseproject/EXEplorer/models.py
+++ src/gseproject/EXEplorer/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     coins = models.IntegerField(default=0)
+    # Add any other fields you need for the user profile
+
+    def __str__(self):
+        return self.user.username + "'s Profile"
