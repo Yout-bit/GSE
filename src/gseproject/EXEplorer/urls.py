e414e1189c02161a9948f0c17139c0939303e6b2
Isaacso <wss207@exeter.ac.uk>
2024-03-19 21:19:56 +0000

    fixing the conflict  #try 1
    fixing the conflict  #try 1

--- src/gseproject/EXEplorer/urls.py
+++ src/gseproject/EXEplorer/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('handle-scanned-number/', handle_scanned_number, name='handle_scanned_number'),
     path('shop/', views.shop_view, name='shop'),
     path('tree/', views.tree_view, name='tree'),
-    path('reset_user_profile/', reset_user_profile, name='reset_user_profile'),
+    path('reset_user_profile', reset_user_profile, name='reset_user_profile'),
     path('api/get_cherries_value/', views.get_cherries_value, name='get_cherries_value'),
     path('api/get_carbon_footprint_value/', views.get_carbon_footprint_value, name='get_carbon_footprint_value'),
 ]
