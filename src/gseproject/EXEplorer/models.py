fa97654069a3eab1970a775325ecee92dde9f279
ncheng <150050119+nchengg@users.noreply.github.com>
2024-03-19 19:19:19 +0000

    Merged currency class into player class, changed username field

--- src/gseproject/EXEplorer/models.py
+++ src/gseproject/EXEplorer/models.py
@@ -1,13 +1,14 @@
 from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Player(models.Model):
-    user = models.CharField(settings.AUTH_USER_MODEL, on_delete = models.CASCADE, verbose_name = "User", related_name = "players")
+    username = models.CharField(settings.AUTH_USER_MODEL, on_delete = models.CASCADE, verbose_name = "User", related_name = "players")
     playerID = models.AutoField(primary_key = True, verbose_name = "Player ID")
-
+    cherry = models.PositiveIntegerField(default = 0, verbose_name = "cherry")
     def __str__(self):
-        return f"{self.user.username} - Player {self.playerID}"
+        return f"{self.username} - Player {self.playerID}"
 
 
 class Tools(models.Model):
@@ -18,16 +19,6 @@ class Tools(models.Model):
     def __str__(self):
         return self.toolsName
 
-
-class Currency(models.Model):
-    player = models.ForeignKey(Player, on_delete = models.CASCADE, verbose_name = "Player", related_name = "currencies")
-    cherry = models.PositiveIntegerField(default = 0, verbose_name = "cherry")
-    
-    def __str__(self):
-        return f"{self.cherry} cherries - {self.player}"
-
-
-
 class Tree(models.Model):
     player = models.ForeignKey(Player, on_delete = models.CASCADE, verbose_name = "Player", related_name = "trees")
     tree = models.PositiveIntegerField(default = 0, verbose_name = "tree")
@@ -35,7 +26,4 @@ class Tree(models.Model):
     
     def str(self):
         return self.tree
-
-
-
     
