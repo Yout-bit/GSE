7b79c95f2096c4b94a5fcc90182a5ca448423d0c
Isaacso <wss207@exeter.ac.uk>
2024-03-21 06:37:38 +0000

    bunch of actions in description
    formating +deleted comments in QRcode.html and views.py, check min and max

--- src/gseproject/EXEplorer/views.py
+++ src/gseproject/EXEplorer/views.py
@@ -39,11 +39,8 @@ def handle_scanned_number(request):
         scanned_number = request.POST.get('scanned_number')
         if scanned_number:
             try:
-                # Retrieve the user profile associated with the current user
-                user_profile = request.user.userprofile
                 
-                # Update the carbonFootprint field with the scanned number
-
+                user_profile = request.user.userprofile
                 user_profile.carbonFootprint += int(scanned_number)
                 user_profile.cherries += 10
                 user_profile.save()
@@ -88,7 +85,7 @@ def get_carbon_footprint_value(request):
 
 def increment_carbon_footprint(request):
     if request.method == 'POST':
-        # Get the current user's profile
+        
         user_profile = request.user.userprofile
         
         # Increment the carbon footprint value by 1
@@ -97,12 +94,6 @@ def increment_carbon_footprint(request):
         # Check if the carbon footprint exceeds 10
         if user_profile.carbonFootprint > 10:
             user_profile.carbonFootprint = 10
-
-        # Check if the carbon footprint goes below 0
-        if user_profile.carbonFootprint < 0:
-            user_profile.carbonFootprint = 0
-
-        # Save the updated profile
         user_profile.save()
         
         # Return a JSON response indicating success and the new carbon footprint value
@@ -113,15 +104,10 @@ def increment_carbon_footprint(request):
 
 def decrement_carbon_footprint(request):
     if request.method == 'POST':
-        # Get the current user's profile
         user_profile = request.user.userprofile
         
-        # Decrement the carbon footprint value by 3
-        user_profile.carbonFootprint -= 3
-
-        # Check if the carbon footprint exceeds 10
-        if user_profile.carbonFootprint > 10:
-            user_profile.carbonFootprint = 10
+        # Decrement the carbon footprint value by 2
+        user_profile.carbonFootprint -= 2
 
         # Check if the carbon footprint goes below 0
         if user_profile.carbonFootprint < 0:
@@ -135,8 +121,8 @@ def decrement_carbon_footprint(request):
     else:
         # Return a JSON response with an error message if the request method is not POST
         return JsonResponse({'success': False, 'error': 'Method not allowed'}, status=405)
+    
 def get_cherries_value(request):
-    # Fetch the current user's cherries value
     cherries_value = request.user.userprofile.cherries
     # Return the value as a JSON response
     return JsonResponse({'cherries_value': cherries_value})
